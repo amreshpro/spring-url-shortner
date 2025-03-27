60b21852def1fa62a9f4bbdbe4dd20bd76f31cd7
Amresh Maurya <amresh.terminal@gmail.com>
2025-03-27 23:16:41 +0530

    add health check

---
+++ src/main/java/com/amreshmaurya/url_shortner/controller/HealthCheckController.java
@@ -0,0 +1,22 @@
+package com.amreshmaurya.url_shortner.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<HashMap<String, String>> healthCheck() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("message", "It works");
+
+        return ResponseEntity.status(HttpStatus.OK).body(map);
+    }
+}
