63d5f045c37eea3ef9260be59cdac19a5dce4f71
Amresh Maurya <amresh.terminal@gmail.com>
2025-03-31 22:29:47 +0530

    add entity

--- src/main/java/com/amreshmaurya/url_shortner/entity/User.java
+++ src/main/java/com/amreshmaurya/url_shortner/entity/User.java
@@ -1,42 +1,32 @@
 package com.amreshmaurya.url_shortner.entity;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "urls")
+@Entity(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class UrlEntity {
+public class User {
     @Id
     @GeneratedValue(generator = "UUID")
     @Column(updatable = false, nullable = false)
     private UUID id;
     
-    @Column(nullable = false, length = 2048)
-    private String originalUrl;
-    
-    @Column(nullable = false, unique = true)
-    private String shortCode;
+    @Column(unique = true, nullable = false)
+    private String username;
     
     @Column(nullable = false)
-    private LocalDateTime createdAt;
+    private String password;
     
     @Column(nullable = false)
-    private LocalDateTime expiresAt;
-    
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private String role;
 }
