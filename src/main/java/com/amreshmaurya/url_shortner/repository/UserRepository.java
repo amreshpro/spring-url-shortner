a3eb8767b79ae57b4612cc9a70fc189815f154e0
Amresh Maurya <amresh.terminal@gmail.com>
2025-03-31 22:35:20 +0530

    add repository

--- src/main/java/com/amreshmaurya/url_shortner/repository/UserRepository.java
+++ src/main/java/com/amreshmaurya/url_shortner/repository/UserRepository.java
@@ -1,17 +1,14 @@
 package com.amreshmaurya.url_shortner.repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.amreshmaurya.url_shortner.entity.UrlEntity;
 import com.amreshmaurya.url_shortner.entity.User;
 
 @Repository
-public interface UrlRepository extends JpaRepository<UrlEntity, UUID> {
-    Optional<UrlEntity> findByShortCode(String shortCode);
-    List<UrlEntity> findByUser(User user);
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
 }
